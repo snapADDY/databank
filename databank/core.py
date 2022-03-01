23ccacec2faf8848ba53da3640989faf04665f08
severinsimmler <s.simmler@snapaddy.com>
2022-03-01 14:56:25 +0100

    chore: remove session

--- databank/core.py
+++ databank/core.py
@@ -18,10 +18,10 @@ class Database:
             URL of the database to connect to.
         """
         self.engine: Engine = create_engine(url, **kwargs)
-        self.registry: ThreadLocalRegistry = scoped_session(sessionmaker(bind=self.engine))
+        self.session: scoped_session = scoped_session(sessionmaker(bind=self.engine))
 
     def execute(self, query: str, params: Mapping = {}):
-        """Execute the given SQL query, optionally bind the params first.
+        """Execute and commit the given SQL query, optionally bind the params first.
 
         Parameters
         ----------
@@ -31,21 +31,23 @@ class Database:
             Parameters to bind to the query.
         """
         # create thread-local session
-        session: Session = self.registry()
+        self.session()
 
         # bind params to sql query
         sql: TextClause = text(query).bindparams(**params)
 
         try:
-            session.execute(sql)
+            self.session.execute(sql)
         except:
-            session.rollback()
+            self.session.rollback()
+            self.session.remove()
             raise
         else:
-            session.commit()
+            self.session.commit()
+            self.session.remove()
 
     def execute_many(self, query: str, params: Iterable[Mapping] = []):
-        """Execute the given SQL query many times, optionally bind the iterable of params first.
+        """Execute and commit multiple SQL queries, optionally bind the iterable of params first.
 
         Parameters
         ----------
@@ -55,18 +57,20 @@ class Database:
             Iterable of params to bind to the query.
         """
         # create thread-local session
-        session: Session = self.registry()
+        self.session()
 
         # construct executable text clause
         sql: TextClause = text(query)
 
         try:
-            session.execute(sql, params=params)
+            self.session.execute(sql, params=params)
         except:
-            session.rollback()
+            self.session.rollback()
+            self.session.remove()
             raise
         else:
-            session.commit()
+            self.session.commit()
+            self.session.remove()
 
     def fetch_one(self, query: str, params: Mapping = {}) -> dict:
         """Execute the given SQL query, optionally bind the params, and fetch the first result.
@@ -79,19 +83,20 @@ class Database:
             Parameters to bind to the query.
         """
         # create thread-local session
-        session: Session = self.registry()
+        self.session()
 
         # bind params to sql query
         sql: TextClause = text(query).bindparams(**params)
 
         try:
-            result: ResultProxy = session.execute(sql)
+            result: ResultProxy = self.session.execute(sql)
             row: RowProxy = result.fetchone()
         except:
-            session.rollback()
+            self.session.rollback()
+            self.session.remove()
             raise
         else:
-            session.commit()
+            self.session.remove()
 
         return dict(row) if row else {}
 
@@ -108,19 +113,20 @@ class Database:
             Number of rows to fetch, by default 1.
         """
         # create thread-local session
-        session: Session = self.registry()
+        self.session()
 
         # bind params to sql query
         sql = text(query).bindparams(**params)
 
         try:
-            proxy: ResultProxy = session.execute(sql)
+            proxy: ResultProxy = self.session.execute(sql)
             result = proxy.fetchmany(n)
         except:
-            session.rollback()
+            self.session.rollback()
+            self.session.remove()
             raise
         else:
-            session.commit()
+            self.session.remove()
 
         return [dict(row) for row in result if row]
 
@@ -135,18 +141,19 @@ class Database:
             Parameters to bind to the query.
         """
         # create thread-local session
-        session: Session = self.registry()
+        self.session()
 
         # bind params to sql query
         sql = text(query).bindparams(**params)
 
         try:
-            proxy: ResultProxy = session.execute(sql)
+            proxy: ResultProxy = self.session.execute(sql)
             result = proxy.fetchall()
         except:
-            session.rollback()
+            self.session.rollback()
+            self.session.remove()
             raise
         else:
-            session.commit()
+            self.session.remove()
 
         return [dict(row) for row in result if row]
