e7123c609905ba4024b3abdaa15fa454678b691f
severinsimmler <s.simmler@snapaddy.com>
2023-01-13 10:14:35 +0100

    feat: compile sql

--- databank/utils.py
+++ databank/utils.py
@@ -1,6 +1,9 @@
 import json
 from datetime import datetime
-from typing import Any, Union
+from typing import Any, Mapping, Union
+from sqlalchemy import text
+from sqlalchemy.sql.elements import TextClause
+from sqlalchemy.engine.interfaces import Dialect
 
 # supported types for a row value
 Value = Union[str, int, float, bool, tuple, datetime]
@@ -55,3 +58,27 @@ def serialize_param(param: Any) -> Value:
         return None
     else:
         raise ValueError(f"{type(param)} is not serializable")
+
+
+def compile_sql(query: str, params: Mapping = {}, dialect: Dialect | None = None) -> str:
+    """Compile the given query and bind the parameters to get the actual SQL query.
+
+    Parameters
+    ----------
+    query : str
+        SQL query to execute.
+    params : Mapping
+        Parameters to bind to the query.
+    dialect : Dialect | None
+        SQL dialect.
+
+    Returns
+    -------
+    str
+        Compile SQL query with actual data.
+    """
+    # bind params to sql query
+    sql: TextClause = text(query).bindparams(**params)
+
+    # compile sql and bind params
+    return str(sql.compile(compile_kwargs={"literal_binds": True}, dialect=dialect))
